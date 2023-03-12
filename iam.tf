5f26b5525b8c114b097c88d57aeb0250ea17c764
Omasan <omasan.edun@gmail.com>
2023-03-12 09:41:23 +0000

    updated iam

--- iam.tf
+++ iam.tf
@@ -1,14 +1,16 @@
 resource "aws_iam_role" "tf-codepipeline-omi-role" {
     name = "tf-codepipeline-omi-role"
-    assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
+    assume_role_policy = <<EOF
+    
+    {
+    "Version": "2012-10-17"
+    "Statement" [
       {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "codepipeline.amazonaws.com"
+        "Action": "sts:AssumeRole"
+        "Effect": "Allow"
+        "Sid": ""
+        "Principal": {
+          "Service": "codepipeline.amazonaws.com"
         }
       },
     ]
