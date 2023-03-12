9ab9735d2013eb450e5badd2fa227f7ec4f0f84b
Omasan <omasan.edun@gmail.com>
2023-03-12 07:52:29 +0000

    updated name main

--- pipeline.tf
+++ pipeline.tf
@@ -70,7 +70,7 @@ resource "aws_codepipeline" "cicd-test-pipeline" {
             output_artifacts = ["tf-code"]
             configuration = {
                 FullRepositoryId = "omasanedun/CICD-AWS-Test-Pipeline"
-                BranchName   = "master"
+                BranchName   = "main"
                 ConnectionArn = var.codestar_connector_credentials
                 OutputArtifactFormat = "CODE_ZIP"
             }
