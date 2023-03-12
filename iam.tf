214d0890afebc9e87f2d9167a5918d6eb4632eca
Omasan <omasan.edun@gmail.com>
2023-03-12 12:52:18 +0000

    updated iam v4

--- iam.tf
+++ iam.tf
@@ -1,36 +1,36 @@
 resource "aws_iam_role" "tf-codepipeline-omi-role" {
     name = "tf-codepipeline-omi-role"
-    assume_role_policy = <<EOF
-    
-    {
-    "Version": "2012-10-17"
-    "Statement" [
+    assume_role_policy = jsonencode({
+        
+    Version = "2012-10-17"
+    Statement = [
       {
-        "Action": "sts:AssumeRole"
-        "Effect": "Allow"
-        "Sid": ""
-        "Principal": {
-          "Service": "codepipeline.amazonaws.com"
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid = ""
+        Principal = {
+          Service = "codepipeline.amazonaws.com"
         }
       },
     ]
   })
+  
 }
 
 data "aws_iam_policy_document" "tf-cicd-pipeline-policies-omi" {
     statement {
       sid = ""
-      actions = [ "coderstar-connections:UseConnection" ]
-      resources = [ "*" ]
+      actions = ["coderstar-connections:UseConnection"]
+      resources = ["*"]
       effect = "Allow"
+    
     }
     statement {
       sid = ""
-      actions = [ "cloudwatch:*", "s3:*", "codebuild:*" ]
-      resources = [ "*" ]
+      actions = ["cloudwatch:*", "s3:*", "codebuild:*"]
+      resources = ["*"]
       effect = "Allow"
     }
-  
 }
 
 resource "aws_iam_policy" "tf-cicd-pipeline-policy-omi" {
@@ -49,21 +49,20 @@ resource "aws_iam_role_policy_attachment" "tf-cicd-pipeline-attachment" {
 resource "aws_iam_role" "tf-codebuild-role" {
   name = "tf-codebuild-role"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
+  assume_role_policy = jsonencode ({
+    
+  Version = "2012-10-17",
+  Statement = [
     {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "codebuild.amazonaws.com"
+      Action = "sts:AssumeRole",
+      Principal = {
+        Service = "codebuild.amazonaws.com"
       },
-      "Effect": "Allow",
-      "Sid": ""
+      Effect = "Allow",
+      Sid = ""
     }
   ]
-}
-EOF
+})
 
 }
 
